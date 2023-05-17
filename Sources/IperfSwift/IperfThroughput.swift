09153818ffb155b3a75d098ef453e861dfd5fe28
sakhabaevegor <sakhabaevegor@gmail.com>
2023-05-17 20:41:02 +0700

    fix: fix kbps, mbps, gbps calculation in Throughput

--- Sources/IperfSwift/IperfThroughput.swift
+++ Sources/IperfSwift/IperfThroughput.swift
@@ -10,17 +10,17 @@ import Foundation
 public struct IperfThroughput {
     public var rawValue: Double
     public var bps: Double {
-        rawValue*8
+        rawValue * 8
     }
     
     public var Kbps: Double {
-        return bps / 1024
+        return bps / 1000
     }
     public var Mbps: Double {
-        return Kbps / 1024
+        return Kbps / 1000
     }
     public var Gbps: Double {
-        return Mbps / 1024
+        return Mbps / 1000
     }
     
     public init(bytesPerSecond initValue: Double) {
